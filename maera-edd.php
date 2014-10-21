bae60f82bb537f52098688c1178a48ef2ad5239f
Aristeides Stathopoulos <aristath@gmail.com>
2014-10-21 19:37:14 +0300

    Declare the plugin & add custom templates path

---
+++ maera-edd.php
@@ -0,0 +1,31 @@
+<?php
+/*
+Plugin Name:         Maera EDD
+Plugin URI:
+Description:         Add support for Easy Digital Downloads to the Maera Framework
+Version:             1.0-dev
+Author:              Aristeides Stathopoulos, Dimitris Kalliris
+Author URI:          http://press.codes
+*/
+
+
+// Exit if accessed directly
+if ( ! defined( 'ABSPATH' ) ) {
+	exit;
+}
+
+// Early exit if EDD is not installed.
+if ( ! class_exists( 'Easy_Digital_Downloads' ) ) {
+	return;
+}
+
+/**
+ * Add the /templates folder for our custom templates
+ */
+function maera_edd_templates_path( $file_paths ) {
+
+	$file_paths[50] = dirname( __FILE__ ) . '/templates';
+	return $file_paths;
+
+}
+add_filter( 'edd_template_paths', 'maera_edd_templates_path' );
