10a1eee1ea869202a7e049e0964e9f4a6feb045b
Aristeides Stathopoulos <aristath@gmail.com>
2014-10-22 19:44:54 +0300

    add a function to return the unformatted value of a download

--- maera-edd.php
+++ maera-edd.php
@@ -43,3 +43,49 @@ function maera_edd_twigs_location( $locations ) {
 
 }
 add_filter( 'maera/timber/locations', 'maera_edd_twigs_location', 1 );
+
+/**
+ * Displays a formatted price for a download
+ *
+ * @since 1.0
+ * @param int $download_id ID of the download price to show
+ * @param bool $echo Whether to echo or return the results
+ * @return void
+ */
+function maera_get_edd_price( $download_id = 0 ) {
+
+	if( empty( $download_id ) ) {
+		$download_id = get_the_ID();
+	}
+
+	if ( edd_has_variable_prices( $download_id ) ) {
+
+		$prices = edd_get_variable_prices( $download_id );
+
+		// Return the lowest price
+		$i = 0;
+		foreach ( $prices as $key => $value ) {
+
+			if( $i < 1 ) {
+				$price = $value['amount'];
+			}
+
+			if ( (float) $value['amount'] < (float) $price ) {
+
+				$price = (float) $value['amount'];
+
+			}
+			$i++;
+		}
+
+		$price = edd_sanitize_amount( $price );
+
+	} else {
+
+		$price = edd_get_download_price( $download_id );
+
+	}
+
+	return $price;
+
+}
