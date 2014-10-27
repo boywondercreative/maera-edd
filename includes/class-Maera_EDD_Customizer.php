3fa3f67935523d9fa83747423ef4f27f4a335905
Aristeides Stathopoulos <aristath@gmail.com>
2014-10-27 18:27:08 +0200

    Add the Easy Digital Downloads section

--- includes/class-Maera_EDD_Customizer.php
+++ includes/class-Maera_EDD_Customizer.php
@@ -6,6 +6,21 @@
 class Maera_EDD_Customizer {
 	
 	function __construct() {
+
+		add_action( 'customize_register', array( $this, 'create_section' ) );
+
+	}
+
+	/*
+	 * Create the section
+	 */
+	function create_section( $wp_customize ) {
+
+		$wp_customize->add_section( 'maera_edd', array(
+			'title'    => __( 'Easy Digital Downloads', 'maera_edd' ),
+			'priority' => 999,
+		) );
+
 	}
 
 }
