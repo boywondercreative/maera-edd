fba048bd9f2cc48d34e5120a5a539cabeade22c2
Aristeides Stathopoulos <aristath@gmail.com>
2014-10-27 18:34:58 +0200

    Default image for downloads control

--- includes/class-Maera_EDD_Customizer.php
+++ includes/class-Maera_EDD_Customizer.php
@@ -8,6 +8,7 @@ class Maera_EDD_Customizer {
 	function __construct() {
 
 		add_action( 'customize_register', array( $this, 'create_section' ) );
+		add_filter( 'kirki/controls', array( $this, 'create_settings' ) );
 
 	}
 
@@ -20,6 +21,26 @@ class Maera_EDD_Customizer {
 			'title'    => __( 'Easy Digital Downloads', 'maera_edd' ),
 			'priority' => 999,
 		) );
+
+	}
+
+	/**
+	 * Create the customizer controls.
+	 * Depends on the Kirki Customizer plugin.
+	 */
+	function create_settings( $controls ) {
+
+		$controls[] = array(
+			'type'        => 'image',
+			'setting'     => 'edd_default_image',
+			'label'       => __( 'Default image for downloads', 'maera_edd' ),
+			'subtitle' => __( 'Upload an image that will be used in case the download does not have a featured image assigned to it.', 'maera_bootstrap' ),
+			'section'     => 'maera_edd',
+			'priority'    => 10,
+			'default'     => null
+		);
+
+		return $controls;
 
 	}
 
