268a0090271ce461ac0b1ff5d719c8bdee191203
Aristeides Stathopoulos <aristath@gmail.com>
2014-11-19 22:33:51 +0200

    bugfix invalid markup

--- templates/widget-cart-item.php
+++ templates/widget-cart-item.php
@@ -1,5 +1,5 @@
 <li class="edd-cart-item row">
 	<div class="[maera_grid_col_8]"><span class="edd-cart-item-title">{item_title}</span></div>
 	<div class="[maera_grid_col_3]"><span class="edd-cart-item-price">&nbsp;{item_amount}&nbsp;</span></div>
-	<div class="[maera_grid_col_1]"><a href="{remove_url}" data-cart-item="{cart_item_id}" data-download-id="{item_id}" data-action="edd_remove_from_cart" class="edd-remove-from-cart red right"><i class="edd-icon-remove"></i><span class="sr-only"<?php _e( 'remove', 'edd' ); ?></span></a></div>
+	<div class="[maera_grid_col_1]"><a href="{remove_url}" data-cart-item="{cart_item_id}" data-download-id="{item_id}" data-action="edd_remove_from_cart" class="edd-remove-from-cart red right"><i class="edd-icon-remove"></i><span class="sr-only"><?php _e( 'remove', 'edd' ); ?></span></a></div>
 </li>
