2fe7c6110cb160df47d619746f10e5dd9b142cbd
Aristeides Stathopoulos <aristath@gmail.com>
2014-12-09 22:08:16 +0200

    Put the main class in the main plugin file

--- maera-edd.php
+++ maera-edd.php
@@ -14,22 +14,60 @@ if ( ! defined( 'ABSPATH' ) ) {
 	exit;
 }
 
-if ( ! defined( 'MAERA_EDD_FILE' ) ) {
-	define( 'MAERA_EDD_FILE', __FILE__ );
-}
+/**
+* The main Maera_EDD class
+*/
+class Maera_EDD {
 
-if ( ! defined( 'MAERA_EDD_PATH' ) ) {
-	define( 'MAERA_EDD_PATH', dirname( __FILE__ ) );
-}
+	private static $instance;
 
-if ( ! defined( 'MAERA_EDD_URL' ) ) {
-	define( 'MAERA_EDD_URL', plugin_dir_url( __FILE__ ) );
+	/**
+	* Get the class instance
+	*/
+	public static function get_instance() {
+
+		if ( null == self::$instance ) {
+			self::$instance = new self;
+		}
+
+		return self::$instance;
+	}
+
+	private function __construct() {
+
+		if ( ! defined( 'MAERA_EDD_FILE' ) ) { define( 'MAERA_EDD_FILE', __FILE__ ); }
+		if ( ! defined( 'MAERA_EDD_PATH' ) ) { define( 'MAERA_EDD_PATH', dirname( __FILE__ ) ); }
+		if ( ! defined( 'MAERA_EDD_URL' ) ) { define( 'MAERA_EDD_URL', plugin_dir_url( __FILE__ ) ); }
+
+		$this->requires();
+		$maera_edd_timber        = new Maera_EDD_Timber();
+		$maera_edd_customizer    = new Maera_EDD_Customizer();
+		$maera_edd_shortcodes    = new Maera_EDD_Shortcodes();
+		$maera_edd_scripts       = new Maera_EDD_Scripts();
+		$maera_edd_mods          = new Maera_EDD_Mods();
+		$maera_edd_compatibility = new Maera_EDD_Compatibility();
+
+	}
+
+	/**
+	* Include any required files
+	*/
+	function requires() {
+
+		require_once( __DIR__ . '/includes/class-Maera_EDD_Timber.php');
+		// require_once( __DIR__ . '/includes/class-Maera_EDD_Tonesque.php');
+		require_once( __DIR__ . '/includes/class-Maera_EDD_Customizer.php');
+		require_once( __DIR__ . '/includes/widgets.php');
+		require_once( __DIR__ . '/includes/class-Maera_EDD_Shortcodes.php');
+		require_once( __DIR__ . '/includes/class-Maera_EDD_Scripts.php');
+		require_once( __DIR__ . '/includes/class-Maera_EDD_Mods.php');
+		require_once( __DIR__ . '/includes/class-Maera_EDD_Compatibility.php');
+
+	}
+
 }
 
 // Load our Maera_EDD class if EDD is installed
 if ( class_exists( 'Easy_Digital_Downloads' ) ) {
-
-	require_once( __DIR__ . '/includes/class-Maera_EDD.php');
 	Maera_EDD::get_instance();
-
 }
