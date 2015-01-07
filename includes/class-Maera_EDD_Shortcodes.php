686c5a96867dc5144bbd20f211d30e40e157f493
Aristeides Stathopoulos <aristath@gmail.com>
2015-01-07 21:24:49 +0200

    fix shortcodes context & rendering

--- includes/class-Maera_EDD_Shortcodes.php
+++ includes/class-Maera_EDD_Shortcodes.php
@@ -4,45 +4,45 @@ class Maera_EDD_Shortcodes {
 
     function __construct() {
         add_filter( 'downloads_shortcode', array( $this, 'modify_edd_download_shortcode' ), 10, 11 );
-		add_action( 'pre_get_posts', array( $this, 'pre_get_posts' ) );
+        add_action( 'pre_get_posts', array( $this, 'pre_get_posts' ) );
     }
 
-	function pre_get_posts() {
+    function pre_get_posts() {
 
-		if ( is_post_type_archive( 'download' ) || is_tax( 'download_category' ) || is_tax( 'download_tag' ) ) {
-			add_filter( 'edd_downloads_query', array( $this, 'archives_query' ) );
-		}
+        if ( is_post_type_archive( 'download' ) || is_tax( 'download_category' ) || is_tax( 'download_tag' ) ) {
+            add_filter( 'edd_downloads_query', array( $this, 'archives_query' ) );
+        }
 
-	}
+    }
 
-	function archives_query( $query ) {
-		global $wp_query;
-		$query_array = (array) $wp_query;
+    function archives_query( $query ) {
+        global $wp_query;
+        $query_array = (array) $wp_query;
 
-		if ( is_tax( 'download_category' ) ) {
-			$term = get_term_by( 'slug', $query_array['query']['download_category'], 'download_category' );
-			$query['tax_query'][] = array(
-				'taxonomy'  => 'download_category',
-				'tax_query' => 'term_id',
-				'terms'     => $term->term_id,
-				'operator'  => 'IN'
-			);
-		} else if ( is_tax( 'download_tag' ) ) {
-			$term = get_term_by( 'slug', $query_array['query']['download_tag'], 'download_tag' );
-			$query['tax_query'][] = array(
-				'taxonomy'  => 'download_tag',
-				'tax_query' => 'term_id',
-				'terms'     => $term->term_id,
-				'operator'  => 'IN'
-			);
-		}
+        if ( is_tax( 'download_category' ) ) {
+            $term = get_term_by( 'slug', $query_array['query']['download_category'], 'download_category' );
+            $query['tax_query'][] = array(
+                'taxonomy'  => 'download_category',
+                'tax_query' => 'term_id',
+                'terms'     => $term->term_id,
+                'operator'  => 'IN'
+            );
+        } else if ( is_tax( 'download_tag' ) ) {
+            $term = get_term_by( 'slug', $query_array['query']['download_tag'], 'download_tag' );
+            $query['tax_query'][] = array(
+                'taxonomy'  => 'download_tag',
+                'tax_query' => 'term_id',
+                'terms'     => $term->term_id,
+                'operator'  => 'IN'
+            );
+        }
 
-		return $query;
-	}
+        return $query;
+    }
 
     /**
-     * Filter [download] shortcode HTML
-     * @since 1.0
+    * Filter [download] shortcode HTML
+    * @since 1.0
     */
     function modify_edd_download_shortcode( $display, $atts, $buy_button, $columns, $column_width, $downloads, $excerpt, $full_content, $price, $thumbnails, $query ) {
 
@@ -76,61 +76,61 @@ class Maera_EDD_Shortcodes {
         ?>
         <div class="downloads-list <?php echo $list_class; ?>">
             [maera_grid_row_open]
-                <?php
+            <?php
 
-                while ( $downloads->have_posts() ) : $downloads->the_post(); $count++;
+            while ( $downloads->have_posts() ) : $downloads->the_post(); $count++;
 
-                    $count       = $count > $columns ? 1 : $count;
-                    $count_class = 1 < $columns ? 'edd-grid-column-' . $rand . '_' . $count : null;
+            $count       = $count > $columns ? 1 : $count;
+            $count_class = 1 < $columns ? 'edd-grid-column-' . $rand . '_' . $count : null;
 
-                    $in_cart         = ( edd_item_in_cart( get_the_ID() ) && ! edd_has_variable_prices( get_the_ID() ) ) ? 'in-cart' : '';
-                    $variable_priced = ( edd_has_variable_prices( get_the_ID() ) ) ? 'variable-priced' : '';
-					$hover_type      = get_theme_mod( 'hover_type', 'edd' );
-					$effect          = 'effect-' . $hover_type;
+            $in_cart         = ( edd_item_in_cart( get_the_ID() ) && ! edd_has_variable_prices( get_the_ID() ) ) ? 'in-cart' : '';
+            $variable_priced = ( edd_has_variable_prices( get_the_ID() ) ) ? 'variable-priced' : '';
+            $hover_type      = get_theme_mod( 'hover_type', 'edd' );
+            $effect          = 'effect-' . $hover_type;
 
-                    $context = Timber::get_context();
-                    $context['post']             = new TimberPost( get_the_ID() );
-                    $context['columns']          = $columns;
-                    $context['display_excerpt']  = ( $excerpt != 'no' && $full_content != 'yes' && has_excerpt() ) ? true : false;
-                    $context['display_full']     = $full_content;
-                    $context['display_buy_btn']  = $buy_button;
-                    $context['in_cart']          = $in_cart;
-                    $context['variable_priced']  = $variable_priced;
-                    $context['column_class']     = $column_class;
-                    $context['count_class']      = $count_class;
-                    $context['count']            = $count;
-                    $context['download_classes'] = array( $in_cart, $variable_priced, $column_class, $count_class, $count, $effect );
-                    $context['btn_class']        = $button_defaults_class;
+            $context = Maera_Template::context();
+            $context['post']             = new TimberPost( get_the_ID() );
+            $context['columns']          = $columns;
+            $context['display_excerpt']  = ( $excerpt != 'no' && $full_content != 'yes' && has_excerpt() ) ? true : false;
+            $context['display_full']     = $full_content;
+            $context['display_buy_btn']  = $buy_button;
+            $context['in_cart']          = $in_cart;
+            $context['variable_priced']  = $variable_priced;
+            $context['column_class']     = $column_class;
+            $context['count_class']      = $count_class;
+            $context['count']            = $count;
+            $context['download_classes'] = array( $in_cart, $variable_priced, $column_class, $count_class, $count, $effect );
+            $context['btn_class']        = $button_defaults_class;
 
-					if ( 1 == $columns ) {
-						$mode = 'list';
-                        $context['download_classes'] = array( $in_cart, $variable_priced, $column_class, $count_class, $count );
-					} else {
-						$mode = $hover_type;
-						$mode = ( 'edd' == $hover_type ) ? 'grid' : $mode;
-					}
-                    Timber::render( array( 'shortcode-download-content-' . $mode . '.twig', ), $context, apply_filters( 'maera/timber/cache', false ) );
+            if ( 1 == $columns ) {
+                $mode = 'list';
+                $context['download_classes'] = array( $in_cart, $variable_priced, $column_class, $count_class, $count );
+            } else {
+                $mode = $hover_type;
+                $mode = ( 'edd' == $hover_type ) ? 'grid' : $mode;
+            }
+            Maera_Template::main( 'shortcode-download-content-' . $mode . '.twig', $context );
 
-                endwhile;
+            endwhile;
 
-                wp_reset_postdata();
-                ?>
+            wp_reset_postdata();
+            ?>
             [maera_grid_container_close]
         </div>
 
-    	<nav id="downloads-shortcode" class="download-navigation clearfix">
-    		<?php
-    		$big = 999999;
-    		echo paginate_links( array(
-    			'base'      => str_replace( $big, '%#%', esc_url( get_pagenum_link( $big ) ) ),
-    			'format'    => '?paged=%#%',
-    			'current'   => max( 1, $query['paged'] ),
-    			'total'     => $downloads->max_num_pages,
-    			'prev_next' => false,
-    			'show_all'  => true,
-    		) );
-    		?>
-    	</nav>
+        <nav id="downloads-shortcode" class="download-navigation clearfix">
+            <?php
+            $big = 999999;
+            echo paginate_links( array(
+            'base'      => str_replace( $big, '%#%', esc_url( get_pagenum_link( $big ) ) ),
+            'format'    => '?paged=%#%',
+            'current'   => max( 1, $query['paged'] ),
+            'total'     => $downloads->max_num_pages,
+            'prev_next' => false,
+            'show_all'  => true,
+            ) );
+            ?>
+        </nav>
         <script>jQuery( "ul.page-numbers" ).addClass( "pagination" );</script>
         <?php
 
