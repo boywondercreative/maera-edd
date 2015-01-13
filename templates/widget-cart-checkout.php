2b267066af71bb9b3eee0adc6f4d36f405b3e558
Aristeides Stathopoulos <aristath@gmail.com>
2015-01-13 19:05:42 +0200

    improvements + bugfixes on the cart widget

--- templates/widget-cart-checkout.php
+++ templates/widget-cart-checkout.php
@@ -1,3 +1,3 @@
 <?php global $edd_options; ?>
 <li class="cart_item edd_subtotal"><?php echo __( 'Subtotal:', 'edd' ). " <span class='subtotal'>" . edd_currency_filter( edd_format_amount( edd_get_cart_subtotal() ) ); ?></span></li>
-<li class="cart_item edd_checkout"><a href="<?php echo edd_get_checkout_uri(); ?>" class="button radius expand <?php echo $edd_options['checkout_color']; ?>"><?php _e( 'Checkout', 'edd' ); ?></a></li>
+<li class="cart_item edd_checkout"><a href="<?php echo edd_get_checkout_uri(); ?>" class="[maera_button_default_medium] radius expand <?php echo $edd_options['checkout_color']; ?>"><?php _e( 'Checkout', 'edd' ); ?></a></li>
